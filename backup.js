4406552ac32717fc9825fd3fe921c1ea4f51da48
ayonalmamun <ayonalmamun@gmail.com>
2020-06-19 21:31:26 +0600

    bug v1

---
+++ backup.js
@@ -0,0 +1,40 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDwoUy2r4q3_cVSf53Qrxa48kP8npdv6QQ",
+    authDomain: "fir-webapp-6614e.firebaseapp.com",
+    databaseURL: "https://fir-webapp-6614e.firebaseio.com",
+    projectId: "fir-webapp-6614e",
+    storageBucket: "fir-webapp-6614e.appspot.com",
+    messagingSenderId: "502109128587",
+    appId: "1:502109128587:web:ae53e5e67a19cf6e3d918e",
+    measurementId: "G-C2PTSPMV0Y"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+// firebase auth persistance
+  firebase.auth.Auth.Persistence.LOCAL;
+  $("#btn-login").click(function(){
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+    if(email != "" && password != ""){
+        var result = firebase.auth().signInWithEmailAndPassword(email, password);
+        
+
+        result.catch(function(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode);
+            console.log(errorMessage);
+            
+            window.alert("Message : " + errorMessage);
+        });
+    }
+    else{
+        window.alert("Form is incomplete. Please fill out all fields.");
+    }
+  });
